e02c486d409e40b505c9cf122b22fe2f7527d955
Luizaprefect <163931342+Luizaprefect@users.noreply.github.com>
2024-05-27 22:26:28 -0300

    Add files via upload

---
+++ LUIZA3D/script.js
@@ -0,0 +1,30 @@
+const caixaPrincipal= document.querySelector(".caixa-principal");
+const caixaPerguntas= document.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector(".caixa-alternativas");
+const caixaResultados= document.querySelector(".caixa-resultados");
+const textoResultado= document.querySelector(".texto-resultados");
+
+const perguntas = [
+   {
+enunciado: "Perguntas 1"
+alternativas:[
+    "Alternativa 1"
+    "Altrnativa 2"  
+]
+ }
+ {
+enunciado: "Perguntas 1"
+alternativas:[
+    "Alternativa 1"
+    "Altrnativa 2"  
+]
+ }
+
+]
+{
+    enunciado: "Perguntas 1"
+    alternativas:[
+        "Alternativa 1"
+        "Altrnativa 2"  
+    ]
+     }
